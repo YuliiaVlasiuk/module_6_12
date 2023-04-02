8529a41a07e5248ac81d65bfd12f63eef0e6a8f3
YuliiaVlasiuk <110493392+YuliiaVlasiuk@users.noreply.github.com>
2023-04-02 13:05:20 +0300

    Initial commit

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,46 @@
+// import { Task } from "components/Task/Task";
+// import css from "./TaskList.module.css";
+
+// export const TaskList = () => {
+//   return (
+//     <ul className={css.list}>
+//       {[].map(task => (
+//         <li className={css.listItem} key={task.id}>
+//           <Task task={task} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+import { Task } from "components/Task/Task";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(state => state.tasks);
+  // Отримуємо значення фільтра із стану Redux
+  const statusFilter = useSelector(state => state.filters.status);
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  return (
+    <ul>
+      {visibleTasks.map(task => (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
